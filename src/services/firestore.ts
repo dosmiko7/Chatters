1a44f613ab173c1f46d1584f317b7cf736e4445f
dosmiko7 <mikolaj.oberda@gmail.com>
2023-11-03 12:22:29 +0100

    feat: added method to search users

--- src/services/firestore.ts
+++ src/services/firestore.ts
@@ -1,20 +1,31 @@
-// Add a second document with a generated ID.
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, getDocs, collection, query, where, or, DocumentData } from "firebase/firestore";
 import { firestore } from "../firebase";
 import { User } from "firebase/auth";
 
 export const addUser = async (user: User) => {
-	const data = {
+	const defaultData = {
 		avatar: "gs://chatters---chat-app.appspot.com/avatars/avatar_ivKwYDsLxLkM34cMKDdw.png",
 		nickname: user.email,
+		email: user.email,
 		friends_list: [],
 	};
 
 	const userIdRef = doc(firestore, "users", user.uid);
 
-	return setDoc(userIdRef, data)
+	return setDoc(userIdRef, defaultData)
 		.then(() => user)
 		.catch((error) => {
 			throw error;
 		});
+};
+
+export const findUser = async (key: string) => {
+	const q = query(collection(firestore, "users"), or(where("nickname", "==", key), where("email", "==", key)));
+
+	const querySnapshot = await getDocs(q);
+	const foundUsers: DocumentData[] = [];
+	querySnapshot.forEach((doc) => {
+		foundUsers.push(doc.data());
+	});
+	console.log(foundUsers);
 };
