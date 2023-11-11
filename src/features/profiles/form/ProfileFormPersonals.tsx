ea5ac2f2650aebbfd576cbabe4e86d8e5a8acf98
dosmiko7 <mikolaj.oberda@gmail.com>
2023-11-11 13:35:01 +0100

    feat: handled errors with validation for PRofileFormPersonals

--- src/features/profiles/form/ProfileFormPersonals.tsx
+++ src/features/profiles/form/ProfileFormPersonals.tsx
@@ -10,51 +10,69 @@ const Grid = styled.div`
 	display: grid;
 	grid-template-columns: repeat(2, 1fr);
 	grid-template-rows: repeat(2, 1fr);
+	gap: 0.6rem;
 `;
 
+const StyledInput = styled(Input)`
+	border-radius: var(--border-radius-xsm);
+	width: 100%;
+	background-color: var(--color-primary-500);
+	margin: var(--padding-xsm);
+
+	&:hover {
+		background-color: transparent;
+	}
+`;
+
+// TODO: Make an error/warning element
 const ProfileFormPersonals = <T extends FieldValues>(props: IProfileFormFieldProps<T>) => {
 	const { register, errors } = props;
-	console.log(errors["name"]?.message); // it works
+
+	const nameValidation = {
+		pattern: { value: /^[a-zA-Z]+$/, message: "Only letters" },
+		minLength: { value: 1, message: "The name should consist of at least 1 character" },
+		maxLength: { value: 20, message: "No more than 20 characters" },
+	};
 
 	return (
 		<div>
 			<Heading as="h3">Personals</Heading>
 			<Grid>
 				<Wrapper>
-					<Input
+					<StyledInput
+						placeholder="Name"
 						type="text"
-						{...register("name" as Path<T>, {
-							pattern: { value: /^[\p{L}]{1,20}$/, message: "Only letters. Maximum 20 characters." },
-						})}
+						{...register("name" as Path<T>, nameValidation)}
 					/>
+					{errors["name"] && <p>{errors["name"].message?.toString()}</p>}
 				</Wrapper>
 				<Wrapper>
-					<Input
+					<StyledInput
+						placeholder="Surname"
 						type="text"
-						{...register("surname" as Path<T>, {
-							pattern: {
-								value: /^[\p{L}]{1,20}$/,
-								message: "Only letters. Maximum 20 characters.",
-							},
-						})}
+						{...register("surname" as Path<T>, nameValidation)}
 					/>
+					{errors["surname"] && <p>{errors["surname"].message?.toString()}</p>}
 				</Wrapper>
 				<Wrapper>
-					<Input
+					<StyledInput
+						placeholder="City"
 						type="text"
 						{...register("city" as Path<T>, {
-							pattern: {
-								value: /^[\p{L}\s]{1,100}$/,
-								message: "Only letters. Maximum 100 characters.",
-							},
+							pattern: { value: /^[a-zA-Z]+$/, message: "Only letters" },
+							minLength: { value: 1, message: "The city should consist of at least 1 character" },
+							maxLength: { value: 100, message: "No more than 100 characters" },
 						})}
 					/>
+					{errors["city"] && <p>{errors["city"].message?.toString()}</p>}
 				</Wrapper>
 				<Wrapper>
-					<Input
-						type="data"
+					<StyledInput
+						placeholder="Birthday"
+						type="date"
 						{...register("birthday" as Path<T>)}
 					/>
+					{errors["data"] && <p>{errors["data"].message?.toString()}</p>}
 				</Wrapper>
 			</Grid>
 		</div>
