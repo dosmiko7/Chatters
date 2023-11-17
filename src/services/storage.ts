d02ab9abec0557861a37ba63895e686732a17eab
dosmiko7 <mikolaj.oberda@gmail.com>
2023-11-17 20:59:15 +0100

    fix: upload Images

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,9 +1,13 @@
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { storage } from "../firebase";
 
+const METADATA = {
+	contentType: "image/png",
+};
+
 export const uploadAvatar = async ({ avatarFile, userId }: { avatarFile: File; userId: string }): Promise<void> => {
 	const avatarRef = ref(storage, `avatars/avatar_${userId}.png`);
-	await uploadBytes(avatarRef, avatarFile);
+	await uploadBytes(avatarRef, avatarFile, METADATA);
 };
 
 export const uploadBackground = async ({
@@ -14,10 +18,10 @@ export const uploadBackground = async ({
 	userId: string;
 }): Promise<void> => {
 	const backgroundRef = ref(storage, `backgrounds/background_${userId}.png`);
-	await uploadBytes(backgroundRef, backgroundFile);
+	await uploadBytes(backgroundRef, backgroundFile, METADATA);
 };
 
 export const getImageURL = async (path: string): Promise<string> => {
-	const result = getDownloadURL(ref(storage, `${path}`));
+	const result = getDownloadURL(ref(storage, path));
 	return result;
 };
