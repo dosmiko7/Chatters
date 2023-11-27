40ca21c259ea669264f4232e369877b4d02c5a14
dosmiko7 <mikolaj.oberda@gmail.com>
2023-11-27 21:34:40 +0100

    refactor: function to fetching gifs

--- src/services/giphy.ts
+++ src/services/giphy.ts
@@ -1,27 +1,22 @@
+import getGifsUrls from "../utils/getGifsUrls";
+
 const giphyKey = "KZ8BFutjfwdu7D9bcSgZqBMD2poam4nM";
 
 export interface IFetchGifsProps {
 	key: string;
 	offset: number;
-	endpoint?: "search" | "random";
 }
 
-const fetchGifs = async ({ key, offset, endpoint }: IFetchGifsProps) => {
+const fetchGifs = async ({ key, offset }: IFetchGifsProps) => {
 	const response = await fetch(
-		`https://api.giphy.com/v1/gifs/${endpoint}?api_key=${giphyKey}&q=${key}&limit=5&offset=${offset}&rating=g&lang=en&bundle=clips_grid_picker`
+		`https://api.giphy.com/v1/gifs/search?api_key=${giphyKey}&q=${key}&limit=5&offset=${offset}&rating=g&lang=en&bundle=clips_grid_picker`
 	).catch((error) => {
 		throw error;
 	});
 	const result = await response.json();
-	const gifs: string[] = [];
-	result.data.map((obj: any) => {
-		gifs.push(obj.images.fixed_width.url);
-	});
+	const gifs = getGifsUrls(result);
 	return gifs;
 };
 
-fetchGifs.defaultValues = {
-	endpoint: "search",
-};
 
 export default fetchGifs;
