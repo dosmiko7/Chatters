64fa5dc5955d88817ebdc7f8aa717d62987ce255
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-02 16:33:22 +0100

    before firestore chats' data structure change

--- src/features/chats/ChatMoreOptions.tsx
+++ src/features/chats/ChatMoreOptions.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Button } from "../../ui/Button";
 import Heading from "../../ui/Heading";
 import { flexRow } from "../../style/Templates";
-import EmojiPicker, { Theme } from "emoji-picker-react";
+import EmojiPicker, { Theme, EmojiClickData } from "emoji-picker-react";
 
 const Option = styled.li`
 	${flexRow}
@@ -13,6 +13,10 @@ const Option = styled.li`
 const ChatMoreOptions = () => {
 	const currentEmoji = "🎨";
 
+	const onEmojiClickHandler = (emoji: EmojiClickData) => {
+		console.log(emoji);
+	};
+
 	return (
 		<div>
 			<Heading as="h3">Chat customization</Heading>
@@ -22,7 +26,10 @@ const ChatMoreOptions = () => {
 					<p>Change emoji</p>
 				</Option>
 			</ul>
-			<EmojiPicker theme={Theme.DARK} />
+			<EmojiPicker
+				onEmojiClick={onEmojiClickHandler}
+				theme={Theme.DARK}
+			/>
 		</div>
 	);
 };
