18dcd3dc50dbf63e1b48809093eadaea2bff7579
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-02 12:35:59 +0100

    feat: added logic to theme changer button

--- src/ui/ThemeChanger.tsx
+++ src/ui/ThemeChanger.tsx
@@ -1,5 +1,24 @@
+import { useContext } from "react";
+import { FaMoon, FaSun } from "react-icons/fa6";
+
+import { DarkThemeContext } from "../context/DarkThemeContext";
+import { Button } from "./Button";
+
 const ThemeChanger = () => {
-	return <div>ThemeChanger</div>;
+	const { isDarkTheme, toggleDarkTheme } = useContext(DarkThemeContext);
+
+	let symbol;
+	if (isDarkTheme) symbol = <FaMoon />;
+	else symbol = <FaSun />;
+
+	return (
+		<Button
+			variant="menu"
+			onClick={toggleDarkTheme}
+		>
+			{symbol}
+		</Button>
+	);
 };
 
 export default ThemeChanger;
