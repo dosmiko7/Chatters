7ad1030deaf11bcd4d6aed6c3d9a2da39ec9fa12
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-03 13:58:37 +0100

    fix: flickering elements entry scroll (changed useEffect to useLayoutEffect)

--- src/features/chats/ChatWindow.tsx
+++ src/features/chats/ChatWindow.tsx
@@ -1,25 +1,30 @@
-import { useRef, useEffect } from "react";
+import { useRef, useLayoutEffect, Suspense } from "react";
 import styled from "styled-components";
 import { toast } from "react-hot-toast";
 
 import { IChatElement } from "./useChat";
 import List from "../../ui/List";
 import ChatMessage from "./ChatMessage";
+import Spinner from "../../ui/Spinner";
 
 const StyledChatWindow = styled.div`
 	height: 100%;
 	flex-grow: 1;
 	padding: 0 5rem;
 	overflow-y: scroll;
+	transform: translateZ(0);
+	-webkit-transform: translateZ(0);
 `;
 
 const ChatWindow = ({ currentUser, chat, error }: { currentUser: string; chat: IChatElement[]; error: boolean }) => {
 	const bottomRef = useRef<null | HTMLDivElement>(null);
 
-	useEffect(() => {
+	useLayoutEffect(() => {
 		const scrollElement = () => {
 			if (bottomRef.current) {
-				bottomRef.current.scrollIntoView();
+				bottomRef.current.scrollIntoView({
+					behavior: "auto",
+				});
 			}
 		};
 		scrollElement();
@@ -29,26 +34,28 @@ const ChatWindow = ({ currentUser, chat, error }: { currentUser: string; chat: I
 
 	return (
 		<StyledChatWindow>
-			<List<IChatElement>
-				data={chat}
-				render={(message: IChatElement, index: number) => {
-					const renderPhoto = index + 1 === chat.length || chat[index].userId !== chat[index + 1].userId;
-					return (
-						<ChatMessage
-							key={message.createdAt.nanoseconds}
-							type={message.type}
-							fileName={message.fileName}
-							currentUser={currentUser}
-							userId={message.userId}
-							renderPhoto={renderPhoto}
-							createdAt={message.createdAt}
-							nickname={message.nickname}
-							avatar={message.avatar}
-							message={message.message}
-						/>
-					);
-				}}
-			/>
+			<Suspense fallback={<Spinner />}>
+				<List<IChatElement>
+					data={chat}
+					render={(message: IChatElement, index: number) => {
+						const renderPhoto = index + 1 === chat.length || chat[index].userId !== chat[index + 1].userId;
+						return (
+							<ChatMessage
+								key={message.createdAt.nanoseconds}
+								type={message.type}
+								fileName={message.fileName}
+								currentUser={currentUser}
+								userId={message.userId}
+								renderPhoto={renderPhoto}
+								createdAt={message.createdAt}
+								nickname={message.nickname}
+								avatar={message.avatar}
+								message={message.message}
+							/>
+						);
+					}}
+				/>
+			</Suspense>
 			<div ref={bottomRef}></div>
 		</StyledChatWindow>
 	);
