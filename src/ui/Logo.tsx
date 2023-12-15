6407deef25014036aa6c5168386bcdc5e4118567
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-15 20:58:14 +0100

    feat: added navigate to dashboard page for main logo

--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { flexCentered } from "../style/Templates";
+import { useNavigate } from "react-router-dom";
 
 const StyledLogo = styled.div`
 	${flexCentered}
@@ -7,14 +8,21 @@ const StyledLogo = styled.div`
 `;
 const Image = styled.img`
 	max-width: 100%;
+
+	&:hover {
+		cursor: pointer;
+	}
 `;
 
 const Logo = () => {
+	const navigate = useNavigate();
+
 	return (
 		<StyledLogo>
 			<Image
 				src="logo.png"
 				alt="Logo"
+				onClick={() => navigate("/dashboard")}
 			/>
 		</StyledLogo>
 	);
