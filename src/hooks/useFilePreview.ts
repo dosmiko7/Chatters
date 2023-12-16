386cc07110bc608df05aa0de48f54c80c0c12cce
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-16 15:52:16 +0100

    refactor: moved useFilePreview

---
+++ src/hooks/useFilePreview.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const useFilePreview = (file: FileList | null) => {
+	const [imgSrc, setImgSrc] = useState<string | null>("");
+
+	useEffect(() => {
+		if (file && file[0]) {
+			if (file[0].type.includes("image")) {
+				const newUrl = URL.createObjectURL(file[0]);
+				setImgSrc(newUrl);
+			}
+		} else setImgSrc(null);
+	}, [file]);
+
+	return { imgSrc };
+};
+
+export default useFilePreview;
