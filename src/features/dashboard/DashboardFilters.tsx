6e0ba9d7243e5fe96207abbe7948d020afa5f008
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-17 15:08:38 +0100

    feat: implemented logic for style to filter buttons

--- src/features/dashboard/DashboardFilters.tsx
+++ src/features/dashboard/DashboardFilters.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { HiSortDescending, HiSortAscending } from "react-icons/hi";
 
@@ -7,18 +8,38 @@ import { Button } from "../../ui/Button";
 const StyledDashboardFilters = styled.div`
 	${flexRow};
 	font-size: 2rem;
+	gap: 1rem;
 	color: var(--font-color);
 `;
 
+const FilterButton = styled(Button)`
+	width: 5rem;
+	height: 5rem;
+`;
+
 const DashboardFilters = () => {
+	const [selectedOption, setSelectedOption] = useState<"asc" | "desc">("desc");
+
+	const onFilterOptionClick = (order: "asc" | "desc") => {
+		setSelectedOption(order);
+	};
+
 	return (
 		<StyledDashboardFilters>
-			<Button variant="menu">
+			<FilterButton
+				variant="menu"
+				style={{ border: `2px solid ${selectedOption === "asc" ? "var(--color-secondary-400)" : "transparent"}` }}
+				onClick={() => onFilterOptionClick("asc")}
+			>
 				<HiSortAscending />
-			</Button>
-			<Button variant="menu">
+			</FilterButton>
+			<FilterButton
+				variant="menu"
+				style={{ border: `2px solid ${selectedOption === "desc" ? "var(--color-secondary-400)" : "transparent"}` }}
+				onClick={() => onFilterOptionClick("desc")}
+			>
 				<HiSortDescending />
-			</Button>
+			</FilterButton>
 		</StyledDashboardFilters>
 	);
 };
