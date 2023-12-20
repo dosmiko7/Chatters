2920c2a0339405c8d59e0f15ce61db48bc62dbd9
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-20 18:31:18 +0100

    feat: added custom hook for removing dashboard's posts

---
+++ src/features/dashboard/useDeletePost.tsx
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { removeDashboardPost } from "../../services/firestore";
+import { toast } from "react-hot-toast";
+
+const useDeletePost = () => {
+	const queryClient = useQueryClient();
+
+	const { mutate: deletePost, status } = useMutation({
+		mutationFn: (postId: string) => removeDashboardPost(postId),
+
+		onSuccess: () => {
+			toast.success("The post has been successfully deleted");
+			queryClient.invalidateQueries({ queryKey: ["posts"] });
+		},
+
+		onError: (error) => {
+			toast.error("Post deletion failed");
+			console.error(error);
+		},
+	});
+
+	return { deletePost, status };
+};
+
+export default useDeletePost;
