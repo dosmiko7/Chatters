e4fb36285cebf4efe09183702803e26caca77442
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-21 18:26:02 +0100

    feat: added template for display message on hover

--- src/style/Templates.ts
+++ src/style/Templates.ts
@@ -54,3 +54,66 @@ export const fileElementStyle = css`
 	width: auto;
 	max-width: 100%;
 `;
+
+type PositionType = "left-top" | "top" | "right-top" | "right" | "right-bottom" | "bottom" | "left-bottom" | "left";
+
+const getPositionStyle = (position: PositionType) => {
+	let style = "";
+
+	switch (position) {
+		case "left-top":
+			style += "top: 0; right: 110%;";
+			style += "transform: translateY(-100%);";
+			break;
+		case "top":
+			style += "top: -10%; left: 50%;";
+			style += "transform: translate(-50%, -100%);";
+			break;
+		case "right-top":
+			style += "top: 0; left: 110%;";
+			style += "transform: translateY(-100%);";
+			break;
+		case "right":
+			style += "top: 50%; left: 110%;";
+			style += "transform: translateY(-50%);";
+			break;
+		case "right-bottom":
+			style += "bottom: 0; left: 110%;";
+			style += "transform: translateY(100%);";
+			break;
+		case "bottom":
+			style += "bottom: -10%; left: 50%;";
+			style += "transform: translate(-50%, 100%);";
+			break;
+		case "left-bottom":
+			style += "bottom: 0; right: 110%;";
+			style += "transform: translateY(100%);";
+			break;
+		case "left":
+			style += "top: 50%; right: 110%;";
+			style += "transform: translateY(-50%);";
+			break;
+		default:
+			break;
+	}
+
+	return css`
+		${style}
+	`;
+};
+
+export const displayInfo = ({ message, position }: { message: string; position: PositionType }) => css`
+	position: relative;
+
+	&:hover::after {
+		content: "${message}";
+		z-index: 1000;
+		position: absolute;
+		${getPositionStyle(position)};
+		width: max-content;
+		background-color: var(--color-primary-500);
+		border: var(--border-thin);
+		padding: var(--padding-xsm);
+		font-weight: var(--font-weight-medium);
+	}
+`;
