b470fcfbd4e7e0d2cfc208671e461cb3f740a2d7
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-22 17:50:10 +0100

    feat: added custom hook useSignOut

---
+++ src/features/authentication/useSignOut.ts
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+import { signOutUser } from "../../services/auth";
+
+const useSignOut = () => {
+	const navigate = useNavigate();
+	const queryClient = useQueryClient();
+
+	const { mutate: signOut, status } = useMutation({
+		mutationFn: () => signOutUser(),
+
+		onSuccess: () => {
+			toast.success("You have logged out successfully");
+			queryClient.removeQueries();
+			navigate("/login", { replace: true });
+		},
+
+		onError: () => {
+			toast.error("Logout failed");
+		},
+	});
+
+	return { signOut, status };
+};
+
+export default useSignOut;
