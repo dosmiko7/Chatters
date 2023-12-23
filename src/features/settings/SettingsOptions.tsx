1358dd080bb6b601c24a822c29a416ea97dbec2f
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-23 20:14:57 +0100

    feat: added card to delete an account

--- src/features/settings/SettingsOptions.tsx
+++ src/features/settings/SettingsOptions.tsx
@@ -1,16 +1,20 @@
 import styled from "styled-components";
 
+import { flexRow } from "../../style/Templates";
 import Options from "../../ui/Options";
 import SettingsOptionsReset from "./SettingsOptionsReset";
-import { flexRow } from "../../style/Templates";
+import SettingsOptionsDelete from "./SettingsOptionsDelete";
 
 const StyledOptionsContainer = styled(Options.Container)`
+	${flexRow};
+	justify-content: center;
 	width: 90%;
 	margin: 5rem auto;
 `;
 
-const StyledOptionsList = styled(Options.List)`
-	${flexRow};
+const StyledOptionsList = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
 	gap: 2rem;
 `;
 
@@ -19,6 +23,7 @@ const SettingsOptions = () => {
 		<StyledOptionsContainer>
 			<StyledOptionsList>
 				<SettingsOptionsReset />
+				<SettingsOptionsDelete />
 			</StyledOptionsList>
 		</StyledOptionsContainer>
 	);
---
+++ src/features/settings/SettingsOptionsDelete.tsx
@@ -0,0 +1,39 @@
+import { FcDisapprove } from "react-icons/fc";
+import { toast } from "react-hot-toast";
+
+import useDeleteAccount from "../authentication/useDeleteAccount";
+import { toasterWarningOptions } from "../../ui/ToasterWarning.options";
+import Card from "../../ui/Card";
+import ToasterWarning from "../../ui/ToasterWarning";
+
+const INFO = "Delete your account permanently. All data will be cleared. It is not possible to recover your account.";
+
+const MESSAGE = "Are you sure you want to delete your account?";
+
+const SettingsOptionsDelete = () => {
+	const { deleteUser } = useDeleteAccount();
+
+	const onDeleteAccount = () => {
+		toast(
+			(t) => (
+				<ToasterWarning
+					t={t}
+					confirmHandler={() => deleteUser()}
+					message={MESSAGE}
+				/>
+			),
+			toasterWarningOptions
+		);
+	};
+
+	return (
+		<Card
+			icon={<FcDisapprove />}
+			heading="Delete account"
+			info={INFO}
+			onClickHandler={onDeleteAccount}
+		/>
+	);
+};
+
+export default SettingsOptionsDelete;
