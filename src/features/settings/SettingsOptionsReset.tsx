719e902aa041bffe8c68fdaab2dd976dce9dcc98
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-23 19:19:24 +0100

    feat: add logic to password reset card

--- src/features/settings/SettingsOptionsReset.tsx
+++ src/features/settings/SettingsOptionsReset.tsx
@@ -1,13 +1,25 @@
 import { FcLock } from "react-icons/fc";
+import { toast } from "react-hot-toast";
 
+import usePasswordReset from "../authentication/usePasswordReset";
+//import useLoggedUser from "../authentication/useLoggedUser";
 import Card from "../../ui/Card";
 
 const INFO =
 	"Click if you want to change your password. We will send you a message to your email with the option to reset your password.";
 
+//TODO: Remove static email
 const SettingsOptionsReset = () => {
+	//const { data } = useLoggedUser();
+	const data = { email: "miko7pl@gmail.com" };
+	const { sendResetEmail } = usePasswordReset();
+
 	const onPasswordReset = () => {
-		console.log("eheh");
+		if (data?.email) {
+			sendResetEmail({ email: data.email });
+		} else {
+			toast.error("There is no email address we can send a message to");
+		}
 	};
 
 	return (
