746d86b7669aae054498d49fc6f1e9f5a7b342fd
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-24 13:19:07 +0100

    feat: added display info for settings button

--- src/ui/Settings.tsx
+++ src/ui/Settings.tsx
@@ -1,18 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import { FaGear } from "react-icons/fa6";
+import styled from "styled-components";
 
+import { displayInfo } from "../style/Templates";
 import Button from "./Button";
+
+const SettingsButton = styled(Button)`
+	${displayInfo({ message: "Settings", position: "right" })}
+`;
 
 const Settings = () => {
 	const navigate = useNavigate();
 
 	return (
-		<Button
+		<SettingsButton
 			variant="menu"
 			onClick={() => navigate("/settings")}
 		>
 			<FaGear />
-		</Button>
+		</SettingsButton>
 	);
 };
 
