c05d28e9737e3d286004423dbef8eaebe2268589
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-25 19:50:39 +0100

    refactor: giphyApi methods

--- src/services/giphy/giphyApi.ts
+++ src/services/giphy/giphyApi.ts
@@ -8,14 +8,16 @@ export interface IFetchGifsProps {
 }
 
 const fetchGifs = async ({ key, offset }: IFetchGifsProps) => {
-	const response = await fetch(
-		`https://api.giphy.com/v1/gifs/search?api_key=${giphyKey}&q=${key}&limit=5&offset=${offset}&rating=g&lang=en&bundle=clips_grid_picker`
-	).catch((error) => {
-		throw error;
-	});
-	const result = await response.json();
-	const gifs = getGifsUrls(result);
-	return gifs;
+	try {
+		const response = await fetch(
+			`https://api.giphy.com/v1/gifs/search?api_key=${giphyKey}&q=${key}&limit=5&offset=${offset}&rating=g&lang=en&bundle=clips_grid_picker`
+		);
+		const result = await response.json();
+		const gifs = getGifsUrls(result);
+		return gifs;
+	} catch {
+		throw new Error("fetchGifs: fetching gifs failed");
+	}
 };
 
 export default fetchGifs;
