e380cd02b7e85ea6347f92619d975fa0a993525c
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-30 00:37:21 +0100

    refactor: changed colors for input

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -3,11 +3,11 @@ import styled from "styled-components";
 const Input = styled.input`
 	border-radius: var(--border-radius-md);
 	padding: var(--padding-sm);
-	background-color: var(--color-primary-25);
+	background-color: var(--color-white-100);
 	border: 1px solid transparent;
+	color: var(--color-black-100);
 
 	&:hover {
-		background-color: var(--color-primary-0);
 		border: 1px solid var(--color-secondary-400);
 	}
 `;
