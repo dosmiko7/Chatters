1b48aba80b4524df1e6f7adb708dfab3c19c647d
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-30 21:50:08 +0100

    refactor: removed navigate

--- src/features/authentication/useOnAuthChange.ts
+++ src/features/authentication/useOnAuthChange.ts
@@ -1,5 +1,4 @@
 import { useQueryClient } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 
@@ -10,20 +9,18 @@ import { updateUserTimestamp } from "../../services/firestore/userApi";
 const useOnAuthChange = () => {
 	const queryClient = useQueryClient();
 	const { loggedUser } = useLoggedUser();
-	const navigate = useNavigate();
 
 	useEffect(() => {
 		const unsub = onAuthStateChanged(auth, async (user) => {
 			if (!user) {
 				await updateUserTimestamp({ mode: "logout", userId: loggedUser?.uid });
 				queryClient.removeQueries();
-				navigate("/login");
 			} else {
 				await updateUserTimestamp({ mode: "login", userId: user.uid });
 			}
 		});
 		return () => unsub();
-	}, [loggedUser, queryClient, navigate]);
+	}, [loggedUser, queryClient]);
 };
 
 export default useOnAuthChange;
