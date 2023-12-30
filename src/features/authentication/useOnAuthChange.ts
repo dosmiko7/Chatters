f8cd83737bd13e7fc183fc1cedf6ee7f4e376fcf
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-30 21:10:19 +0100

    refactor: removed beforeAuthStateChanged

--- src/features/authentication/useOnAuthChange.ts
+++ src/features/authentication/useOnAuthChange.ts
@@ -1,7 +1,7 @@
 import { useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import { beforeAuthStateChanged, onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 
 import useLoggedUser from "./useLoggedUser";
 import { auth } from "../../firebase";
@@ -13,25 +13,17 @@ const useOnAuthChange = () => {
 	const navigate = useNavigate();
 
 	useEffect(() => {
-		const unsub = beforeAuthStateChanged(auth, async (user) => {
-			if (user) {
-				await updateUserTimestamp({ mode: "login", userId: user.uid });
-			} else {
-				await updateUserTimestamp({ mode: "logout", userId: loggedUser?.uid });
-			}
-		});
-		return () => unsub();
-	}, [loggedUser]);
-
-	useEffect(() => {
-		const unsub = onAuthStateChanged(auth, (user) => {
+		const unsub = onAuthStateChanged(auth, async (user) => {
 			if (!user) {
+				await updateUserTimestamp({ mode: "logout", userId: loggedUser?.uid });
 				queryClient.removeQueries();
 				navigate("/login");
+			} else {
+				await updateUserTimestamp({ mode: "login", userId: user.uid });
 			}
 		});
 		return () => unsub();
-	}, [queryClient, navigate]);
+	}, [loggedUser, queryClient, navigate]);
 };
 
 export default useOnAuthChange;
