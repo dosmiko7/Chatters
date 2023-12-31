37e4966f31f11e3f0e74739127ee632b0d3cda3b
dosmiko7 <mikolaj.oberda@gmail.com>
2023-12-31 14:22:32 +0100

    refactor: added :focus class; changed background color

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -3,12 +3,16 @@ import styled from "styled-components";
 const Input = styled.input`
 	border-radius: var(--border-radius-md);
 	padding: var(--padding-sm);
-	background-color: var(--color-white-100);
+	background-color: var(--color-primary-500);
 	border: 1px solid transparent;
 	color: var(--font-color);
 
 	&:hover {
 		border: 1px solid var(--color-secondary-400);
+	}
+
+	&:focus {
+		background-color: transparent;
 	}
 `;
 
