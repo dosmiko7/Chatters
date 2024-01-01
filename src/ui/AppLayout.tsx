124d0ea66509a918a793da587354fdd6772af3fd
dosmiko7 <mikolaj.oberda@gmail.com>
2024-01-01 17:24:17 +0100

    feat: added breakpoint for phone devices; changed size of elements

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -7,24 +7,37 @@ import ChatsBar from "../features/chats/chatsList/ChatsBar";
 const StyledAppLayout = styled.div`
 	display: grid;
 	grid-template-columns: 0.6fr minmax(0, 3fr) 11fr;
-	max-width: 100vw;
+	max-width: 100dvw;
+	height: 100dvh;
+	max-height: 100dvh;
 
 	@media only screen and (width <= 1000px) {
 		grid-template-rows: 0.6fr 11fr;
 		grid-template-columns: 3fr 11fr;
+	}
+
+	@media only screen and (width <= 680px) {
+		grid-template-rows: 0.6fr 1fr 11fr;
+		grid-template-columns: 1fr;
 	}
 `;
 
 const Main = styled.div`
 	position: relative;
 	background-color: var(--color-primary-400);
-	height: 100vh;
-	max-height: 100vh;
+	height: 100%;
+	max-height: 100%;
+	max-width: 100%;
 	overflow-y: scroll;
 
 	@media only screen and (width <= 1000px) {
 		grid-row: 2/3;
 		grid-column: 2/3;
+	}
+
+	@media only screen and (width <= 680px) {
+		grid-row: 3/4;
+		grid-column: 1/-1;
 	}
 `;
 
