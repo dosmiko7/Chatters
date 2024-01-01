603dab58dc8e82c660a8c0df043e6743ae1d7aab
dosmiko7 <mikolaj.oberda@gmail.com>
2024-01-01 17:23:14 +0100

    feat: handle responsiveness

--- src/features/chats/more/ChatMoreOptions.tsx
+++ src/features/chats/more/ChatMoreOptions.tsx
@@ -1,12 +1,26 @@
+import styled from "styled-components";
+
+import { flexColumn } from "../../../style/Templates";
 import ChatMoreCustomization, { ICustomizationProps } from "./ChatMoreCustomization";
 import ChatMorePrivacy from "./ChatMorePrivacy";
 
+const StyledChatMoreOptions = styled.div`
+	${flexColumn};
+
+	@media only screen and (width <= 1000px) {
+		font-size: 1.8rem;
+		h3 {
+			font-size: 2rem;
+		}
+	}
+`;
+
 const ChatMoreOptions = ({ customizationData }: { customizationData: ICustomizationProps }) => {
 	return (
-		<>
+		<StyledChatMoreOptions>
 			<ChatMoreCustomization data={customizationData} />
 			<ChatMorePrivacy />
-		</>
+		</StyledChatMoreOptions>
 	);
 };
 
