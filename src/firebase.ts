38375f996d25e1bfb6ff636c51586c59fd404d51
dosmiko7 <mikolaj.oberda@gmail.com>
2024-01-02 20:08:33 +0100

    feat: moved api keys to env file

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,21 +3,15 @@ import { Auth, getAuth } from "firebase/auth";
 import { Firestore, getFirestore } from "firebase/firestore";
 import { FirebaseStorage, getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 // TODO: Change rules for firestore and storage
-// TODO: Change these keys somehow for private
 const firebaseConfig = {
-	apiKey: "AIzaSyANRqxp9hKPcBlENbxeicY1dynC_8vta2c",
-	authDomain: "chatters---chat-app.firebaseapp.com",
-	projectId: "chatters---chat-app",
-	storageBucket: "chatters---chat-app.appspot.com",
-	messagingSenderId: "385913650244",
-	appId: "1:385913650244:web:72634101766d5a66485011",
-	measurementId: "G-7PZ45VFN9K",
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT,
 };
 
 const app: FirebaseApp = initializeApp(firebaseConfig);
--- src/services/giphy/giphyApi.ts
+++ src/services/giphy/giphyApi.ts
@@ -1,7 +1,5 @@
 import getGifsUrls from "../../utils/getGifsUrls";
 
-const giphyKey = "KZ8BFutjfwdu7D9bcSgZqBMD2poam4nM";
-
 export interface IFetchGifsProps {
 	key: string;
 	offset: number;
@@ -10,7 +8,9 @@ export interface IFetchGifsProps {
 const fetchGifs = async ({ key, offset }: IFetchGifsProps) => {
 	try {
 		const response = await fetch(
-			`https://api.giphy.com/v1/gifs/search?api_key=${giphyKey}&q=${key}&limit=5&offset=${offset}&rating=g&lang=en&bundle=clips_grid_picker`
+			`https://api.giphy.com/v1/gifs/search?api_key=${
+				import.meta.env.VITE_GIPHY_API_KEY
+			}&q=${key}&limit=5&offset=${offset}&rating=g&lang=en&bundle=clips_grid_picker`
 		);
 		const result = await response.json();
 		const gifs = getGifsUrls(result);
