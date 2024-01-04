d0fb658cf070e2ebe35a72dce4c9b01bcdcdaa21
dosmiko7 <mikolaj.oberda@gmail.com>
2024-01-04 19:34:58 +0100

    feat: disable delete option for test user

--- src/features/settings/reauthenticate/SettingsReauthenticateModal.tsx
+++ src/features/settings/reauthenticate/SettingsReauthenticateModal.tsx
@@ -6,7 +6,14 @@ import Card from "../../../ui/Card";
 
 const INFO = "Delete your account permanently. All data will be cleared. It is not possible to recover your account.";
 
+// INFO: DELETE OPTION IS DISABLED FOR TEST ACCOUNT
+import useLoggedUser from "../../authentication/useLoggedUser";
+
 const SettingsReauthenticateModal = () => {
+	const { loggedUser } = useLoggedUser();
+
+	const info = <div>Delete option is disabled for test user</div>;
+
 	return (
 		<Modal>
 			<Modal.Open opens="reauthenticate">
@@ -23,7 +30,7 @@ const SettingsReauthenticateModal = () => {
 				name="reauthenticate"
 				height="fit-content"
 			>
-				<SettingsReauthenticate />
+				{loggedUser?.email === "cidot77582@vkr1.com" ? info : <SettingsReauthenticate />}
 			</Modal.Window>
 		</Modal>
 	);
