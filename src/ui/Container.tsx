4e2a3d049c766f41b14b710cb0c6fe61e8019d8d
dosmiko7 <mikolaj.oberda@gmail.com>
2024-01-08 20:34:56 +0100

    feat: added role

--- src/ui/Container.tsx
+++ src/ui/Container.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
-const Container = styled.div`
+const Container = styled.div.attrs({
+	role: "container",
+})`
 	background-color: var(--color-primary-400);
 	padding: var(--padding-sm);
 	border-right: 1px solid var(--color-primary-200);
