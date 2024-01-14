1f7a1b05532d0d867b037e1b502c1ecacd283f2a
dosmiko7 <mikolaj.oberda@gmail.com>
2024-01-14 21:55:58 +0100

    fix: handle objects without certain property

--- src/utils/getGifsUrls.ts
+++ src/utils/getGifsUrls.ts
@@ -1,7 +1,10 @@
 const getGifsUrls = (giphyResult: any) => {
 	const gifsUrls: string[] = [];
 	giphyResult.data.map((obj: any) => {
-		gifsUrls.push(obj.images.fixed_width.url);
+		const url = obj.images?.fixed_width?.url;
+		if (url) {
+			gifsUrls.push(url);
+		}
 	});
 	return gifsUrls;
 };
