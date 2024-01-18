842f55041ac513ebb93a9ca50d526af33a009100
dosmiko7 <mikolaj.oberda@gmail.com>
2024-01-18 14:18:09 +0100

    test: unrelated tests removed

--- src/features/profiles/form/__tests__/ProfileFormSocials.test.tsx
+++ src/features/profiles/form/__tests__/ProfileFormSocials.test.tsx
@@ -1,59 +1,8 @@
-import { render, screen, waitFor, within } from "@testing-library/react";
-import { userEvent } from "@testing-library/user-event";
+import { render, screen, within } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 
 import ProfileFormSocials from "../ProfileFormSocials";
 import Wrapper from "./formWrapper";
-
-/*
-const ProfileFormSocials = () => {
-	const { register, formState } = useFormContext();
-	const socialsErrors = formState.errors.socials;
-
-	return (
-		<StyledSocials>
-			<Heading as="h3">Socials</Heading>
-			<FlexColumn>
-				<FlexColumn aria-label="Linkedin input section">
-					<InputContainer>
-						<FaLinkedin style={iconStyle} />
-						<StyledInput
-							type="url"
-							placeholder="Linkedin"
-							{...register("socials.linkedin", linkValidation)}
-						/>
-					</InputContainer>
-					{socialsErrors && <ErrorMessage>{get(socialsErrors, "linkedin")?.message}</ErrorMessage>}
-				</FlexColumn>
-
-				<FlexColumn aria-label="GitHub input section">
-					<InputContainer>
-						<FaGithub style={iconStyle} />
-						<StyledInput
-							type="url"
-							placeholder="Github"
-							{...register("socials.github", linkValidation)}
-						/>
-					</InputContainer>
-					{socialsErrors && <ErrorMessage>{get(socialsErrors, "github")?.message}</ErrorMessage>}
-				</FlexColumn>
-
-				<FlexColumn aria-label="Twitter input section">
-					<InputContainer>
-						<FaXTwitter style={iconStyle} />
-						<StyledInput
-							type="url"
-							placeholder="Twitter"
-							{...register("socials.twitter", linkValidation)}
-						/>
-					</InputContainer>
-				</FlexColumn>
-				{socialsErrors && <ErrorMessage>{get(socialsErrors, "twitter")?.message}</ErrorMessage>}
-			</FlexColumn>
-		</StyledSocials>
-	);
-};
-*/
 
 describe("ProfileFormSocials", () => {
 	test("render properly", () => {
@@ -73,25 +22,5 @@ describe("ProfileFormSocials", () => {
 		const twitterSection = screen.getByLabelText(/^Twitter/i);
 		expect(twitterSection).toBeInTheDocument();
 		expect(within(twitterSection).getByRole("textbox")).toBeInTheDocument();
-	});
-
-	test("should display matching error while input is invalid", async () => {
-		render(<ProfileFormSocials />, { wrapper: Wrapper });
-
-		const linkedinSection = screen.getByLabelText(/^Linkedin/i);
-		const input = within(linkedinSection).getByRole("textbox");
-
-		await userEvent.type(input, "invalid");
-		await waitFor(() => expect(screen.getByText("Invalid URL format")).toBeInTheDocument());
-	});
-
-	test("should not display error if input is valid", async () => {
-		render(<ProfileFormSocials />, { wrapper: Wrapper });
-
-		const twitterSection = screen.getByLabelText(/^Twitter/i);
-		const input = within(twitterSection).getByRole("textbox");
-
-		await userEvent.type(input, "https://twitter.com");
-		await waitFor(() => expect(screen.queryByText("Invalid URL format")).not.toBeInTheDocument());
 	});
 });
