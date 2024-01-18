fbbbcf056b661d1ca73ce82deae97fc5122dce0d
dosmiko7 <mikolaj.oberda@gmail.com>
2024-01-18 18:16:00 +0100

    feat: moved fileValidation

--- src/utils/validationTemplates.ts
+++ src/utils/validationTemplates.ts
@@ -1,3 +1,5 @@
+import isFileExtensionValid from "./isFileExtensionValid";
+
 export const emailValidation = {
 	required: "Email is required",
 	pattern: {
@@ -48,4 +50,22 @@ export const dateValidation = {
 	validate: (date: Date) => {
 		return date < new Date() || "The date should not be newer than today";
 	},
+};
+
+export const fileValidation = (value: FileList | null) => {
+	if (value?.length) {
+		const file = value[0];
+		const maxSizeInBytes = 1024 * 1024;
+		const allowedExtensions = ["jpg", "jpeg", "png"];
+
+		if (!isFileExtensionValid(file.name, allowedExtensions)) {
+			return "Only JPG and PNG are allowed.";
+		}
+
+		if (file.size > maxSizeInBytes) {
+			return "File size should be less than 1 MB";
+		}
+	}
+
+	return true;
 };
