5dc176509a70123b3f26cf0b7a145eb11a8e1315
dosmiko7 <mikolaj.oberda@gmail.com>
2024-01-20 17:43:34 +0100

    test: tests for DashboardDetail component

---
+++ src/features/dashboard/__tests__/DashboardDetail.test.tsx
@@ -0,0 +1,29 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+
+import DashboardDetail from "../DashboardDetail";
+
+vi.mock("../DashboardButtons");
+vi.mock("../DashboardList");
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			retry: false,
+		},
+	},
+});
+
+const wrapper = ({ children }: { children: JSX.Element }) => {
+	return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+};
+
+describe("DashboardDetail", () => {
+	test("render properly", () => {
+		render(<DashboardDetail />, { wrapper });
+
+		expect(screen.getByRole("container")).toBeInTheDocument();
+		expect(screen.getByRole("heading").textContent).toBe("Dashboard");
+	});
+});
