ed961c2c90fde37e5a0eb4378ff68670f6c9717e
dosmiko7 <mikolaj.oberda@gmail.com>
2024-01-20 17:13:12 +0100

    test: tests for DashboardFilters component

---
+++ src/features/dashboard/__tests__/DashboardFilters.test.tsx
@@ -0,0 +1,103 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import { userEvent } from "@testing-library/user-event";
+
+import * as useDashboardOptionsHooks from "../../../context/useDashboardOptions";
+import { IOptionsDashboard } from "../../../services/firestore/dashboardApi";
+import DashboardOptionsProvider from "../../../context/DashboardOptionsContext";
+import DashboardSortFilters from "../DashboardFilters";
+
+vi.mock("react-icons/hi", () => {
+	const actual = vi.importActual("react-icons/hi");
+	return {
+		...actual,
+		HiSortAscending: () => <div>HiSortAscending</div>,
+		HiSortDescending: () => <div>HiSortDescending</div>,
+	};
+});
+
+vi.mock("../DashboardFilterButton", () => {
+	return {
+		default: (props: any) => (
+			<button onClick={props.onClickHandler}>
+				{props.icon}
+				<span>{props.isActive.toString()}</span>
+			</button>
+		),
+	};
+});
+
+const wrapper = ({ children }: { children: JSX.Element }) => {
+	return <DashboardOptionsProvider>{children}</DashboardOptionsProvider>;
+};
+
+describe("DashboardFilters", () => {
+	const setOrderMock = vi.fn();
+	const setKeyMock = vi.fn();
+
+	test("render properly", () => {
+		render(<DashboardSortFilters />, { wrapper });
+
+		const buttons = screen.getAllByRole("button");
+		expect(buttons.length).toBe(2);
+		expect(screen.getByText("HiSortAscending")).toBeInTheDocument();
+		expect(screen.getByText("HiSortDescending")).toBeInTheDocument();
+	});
+
+	test("should call setOrder with asc on button with HiSortAscending icon", async () => {
+		vi.spyOn(useDashboardOptionsHooks, "default").mockReturnValue({
+			options: {} as IOptionsDashboard,
+			setOrder: setOrderMock,
+			setKey: setKeyMock,
+		});
+
+		render(<DashboardSortFilters />, { wrapper });
+
+		const buttonAsc = screen.getByRole("button", { name: "HiSortAscending false" });
+
+		await userEvent.click(buttonAsc);
+
+		expect(setOrderMock).toBeCalledWith("asc");
+	});
+
+	test("should call setOrder with desc on button with HiSortDescending icon", async () => {
+		vi.spyOn(useDashboardOptionsHooks, "default").mockReturnValue({
+			options: {} as IOptionsDashboard,
+			setOrder: setOrderMock,
+			setKey: setKeyMock,
+		});
+
+		render(<DashboardSortFilters />, { wrapper });
+
+		const buttonDesc = screen.getByRole("button", { name: "HiSortDescending true" });
+
+		await userEvent.click(buttonDesc);
+
+		expect(setOrderMock).toBeCalledWith("desc");
+	});
+
+	test("updates selectedOption state correctly", async () => {
+		vi.spyOn(useDashboardOptionsHooks, "default").mockReturnValue({
+			options: {} as IOptionsDashboard,
+			setOrder: setOrderMock,
+			setKey: setKeyMock,
+		});
+
+		render(<DashboardSortFilters />, { wrapper });
+
+		const buttonDesc = screen.getByRole("button", { name: "HiSortDescending true" });
+		const buttonAsc = screen.getByRole("button", { name: "HiSortAscending false" });
+
+		await userEvent.click(buttonDesc);
+		await waitFor(() => {
+			expect(buttonDesc).toHaveTextContent("true");
+			expect(buttonAsc).toHaveTextContent("false");
+		});
+
+		await userEvent.click(buttonAsc);
+		await waitFor(() => {
+			expect(buttonAsc).toHaveTextContent("true");
+			expect(buttonDesc).toHaveTextContent("false");
+		});
+	});
+});
