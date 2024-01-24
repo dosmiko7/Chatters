f211a725408cc85ea52c506f0cf6092dc16fb8eb
dosmiko7 <mikolaj.oberda@gmail.com>
2024-01-24 20:38:35 +0100

    test: tests for ChatMoreOptions component

---
+++ src/features/chats/more/__tests__/ChatMoreOptions.test.tsx
@@ -0,0 +1,37 @@
+import { screen, render } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+
+import ChatMoreOptions from "../ChatMoreOptions";
+
+vi.mock("../ChatMoreCustomization", () => {
+	return {
+		default: (props: any) => <div data-testid="ChatMoreCustomization">{JSON.stringify(props.data)}</div>,
+	};
+});
+
+vi.mock("../ChatMorePrivacy", () => {
+	return {
+		default: () => <div data-testid="ChatMorePrivacy"></div>,
+	};
+});
+
+describe("ChatMoreOptions", () => {
+	const testData = {
+		setTheme: "testTheme",
+		setEmoji: "testEmoji",
+	};
+
+	test("render properly", () => {
+		render(<ChatMoreOptions customizationData={testData} />);
+
+		expect(screen.getByTestId("ChatMoreCustomization")).toBeInTheDocument();
+		expect(screen.getByTestId("ChatMorePrivacy")).toBeInTheDocument();
+	});
+
+	test("pass props to children correctly", () => {
+		render(<ChatMoreOptions customizationData={testData} />);
+
+		const chatMoreCustomization = screen.getByTestId("ChatMoreCustomization");
+		expect(chatMoreCustomization).toHaveTextContent(JSON.stringify(testData));
+	});
+});
